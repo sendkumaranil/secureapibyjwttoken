96632fb264d7d6116d17334c2d3f771b0e83bdb2
Anil Kumar <anilkumar@Anils-MacBook-Air.local>
2019-12-18 00:42:05 +0530

    Create Intial Plain Spring Boot Project

---
+++ src/main/java/com/secure/api/springbootsecurity/jwttoken/SecureapibyjwttokenApplication.java
@@ -0,0 +1,13 @@
+package com.secure.api.springbootsecurity.jwttoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureapibyjwttokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureapibyjwttokenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/secure/api/springbootsecurity/jwttoken/SecureapibyjwttokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secure.api.springbootsecurity.jwttoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureapibyjwttokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
