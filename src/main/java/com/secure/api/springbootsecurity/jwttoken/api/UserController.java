a14c36a3c71baf9867155b5d146f8a916621fe4c
Anil Kumar <anilkumar@Anils-MacBook-Air.local>
2019-12-18 02:55:54 +0530

    Added validation for username creation

--- src/main/java/com/secure/api/springbootsecurity/jwttoken/api/UserController.java
+++ src/main/java/com/secure/api/springbootsecurity/jwttoken/api/UserController.java
@@ -3,6 +3,8 @@ package com.secure.api.springbootsecurity.jwttoken.api;
 import com.secure.api.springbootsecurity.jwttoken.entites.ApplicationUser;
 import com.secure.api.springbootsecurity.jwttoken.repositories.ApplicationUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,8 +22,19 @@ public class UserController {
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @PostMapping("/sign-up")
-    public void signUp(@RequestBody ApplicationUser user){
-        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-        applicationUserRepository.save(user);
+    public ResponseEntity<String> signUp(@RequestBody ApplicationUser user){
+        if(!isUserExists(user)) {
+            user.setUsername(user.getUsername().toLowerCase());
+            user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+            applicationUserRepository.save(user);
+            return new ResponseEntity<>("Username "+user.getUsername()+" Created",HttpStatus.CREATED);
+        }else{
+            return new ResponseEntity<>("Username already exists", HttpStatus.FOUND);
+        }
+    }
+
+    private boolean isUserExists(ApplicationUser user){
+
+        return applicationUserRepository.findByUsername(user.getUsername().toLowerCase())!=null?true:false;
     }
 }
